894f0c7b9221fe914c7e4eeacbe499cf6fb76cad
Rajesh18rj <rajeshkumar18rj@gmail.com>
2025-11-28 12:45:37 +0530

    first commit

---
+++ resources/views/components/footer-sub.blade.php
@@ -0,0 +1,12 @@
+<!-- Bottom bar footer -->
+<div class="border-t border-[#e2d3aa] mt-0 bg-[#fbeac2]">
+    <div class="max-w-6xl mx-auto px-4 py-4 flex flex-col md:flex-row items-center justify-between text-xs text-[#6b7b5a] gap-2">
+        <p>Copyright © {{ date('Y') }} OneLastTry. All Rights Reserved</p>
+        <p>
+            Designed &amp; Developed by
+            <a href="https://do365tech.com/" class="font-semibold hover:text-[#f7921e]" target="_blank" rel="noopener">
+                DO365 Technologies
+            </a>
+        </p>
+    </div>
+</div>
